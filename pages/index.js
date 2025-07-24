38abbd1b111dae631f653d2d7ad37e3f7861cd2e
Reyansh3157 <ordy.3157@gmail.com>
2025-07-24 22:26:52 +0700

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,26 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Layout from "../components/layout";
-import Header from "../components/header";
+import Link from "next/link";
 
 export default function LoginPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter();
-
-  const handleLogin = async () => {
-    const res = await fetch("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
-    });
-
-    const data = await res.json();
-    if (res.ok) {
-      alert("Login successful");
-      router.push("/dashboard"); // redirect after login
-    } else {
-      alert(data.message || "Login failed");
-    }
-  };
-
+  
   return <Layout>
     <div style={styles.container}>
       <img src="act+ logo.png" alt="Logo" style={styles.logo} />
       <input
         type="text"
         placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        
         style={styles.input}
       />
       <input
         type="password"
         placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        
         style={styles.input}
       />
-      <button onClick={handleLogin} style={styles.button}>
+      <button style={styles.button}>
         Login
       </button>
     </div>
